8c2f920c1f2b3eba9e2860765c94f6f25c0f9332
Dav!_mentecapto <daviruasalmeidabr2000@gmail.com>
2025-09-19 21:56:44 -0300

    mais progresso
    mais ou menos pq o js do desafio 3 nao funciona

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function changeColor(color) {
     let h1 = document.querySelector("#h1");
     h1.style.color = color;
 }
+
+function changeBackgroundColor(){
+    let color = document.getElementById("colorInput").value.toLowerCase();
+    let body = document.body;
+    body.style.backgroundColor = color;
+}
